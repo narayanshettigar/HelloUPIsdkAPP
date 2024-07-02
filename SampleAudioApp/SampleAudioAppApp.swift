abdc71eff011042c7972241ae4dbb63809e458c1
Narayan Shettigar <narayan.p@cumualtions.com>
2024-07-02 17:23:43 +0530

    Initial Commit

---
+++ SampleAudioApp/SampleAudioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAudioAppApp.swift
+//  SampleAudioApp
+//
+//  Created by Narayan Shettigar on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleAudioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
